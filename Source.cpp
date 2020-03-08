55893c8a35e95fec1ce32e66f83b82b20b60fe53
innaanikina <avril-avril@mail.ru>
2020-03-09 00:14:29 +0500

    added default allocator

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,14 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
 #include <iostream>
 #include <iomanip>
 #include <time.h>
 #include "windows.h"
+#include <map>
 
+#define _CRT_SECURE_NO_WARNING
 using namespace std;
 
 int main()
@@ -13,6 +19,32 @@ int main()
 
 	cout << "The book's size is: " << size.QuadPart << endl;
 
+	DWORD bytesToRead;
+	char* Text = new char[size.QuadPart + 1];
+	ReadFile(book, Text, size.QuadPart, &bytesToRead, NULL);
+	CloseHandle(book);
+
+	map<string, size_t> words;
+	char* word = strtok(Text, " .,-\n\t\r:;!?()"); 
+	while (word != NULL)
+	{
+		words[word]++;
+		word = strtok(NULL, " .,-\n\t\r:;!?()=\"");
+	}
+
+	map<int, string> result;
+
+	for (auto it = words.begin(); it != words.end(); ++it)
+	{
+		result[(*it).second] = (*it).first;
+	}
+
+	for (auto iter = result.begin(); iter != result.end(); ++iter)
+	{
+		cout << (*iter).first << " : " << (*iter).second << endl;
+	}
+
+	return 0;
 
 
 }
